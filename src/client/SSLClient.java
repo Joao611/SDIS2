30027154b683c2daa006b333532a3151527a1b46
Joao611 <joaofvde@gmail.com>
2018-04-11 12:02:06 +0100

    Server receiving and sending, client started

--- src/client/SSLClient.java
+++ src/client/SSLClient.java
@@ -1,5 +1,87 @@
 package client;
 
-public class SSLClient {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
 
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+
+public class SSLClient {
+	
+	private InetAddress host;
+	private int port;
+	private String operation;
+	private String plateNumber;
+	private String ownerName;
+	private ArrayList<String> wantedCipherSuites = new ArrayList<String>();
+	
+	SSLSocket socket;
+	PrintWriter out = null;
+    BufferedReader in = null;
+
+	public SSLClient(String[] args) throws IOException {
+		parseArgs(args);
+		initSocket();
+	}
+
+	private void initSocket() throws IOException {
+		SSLSocketFactory sslsocketfactory = (SSLSocketFactory)SSLSocketFactory.getDefault();
+		socket = (SSLSocket) sslsocketfactory.createSocket(host, port);
+		
+		// Initializing the streams for Communication with the Server
+     	out = new PrintWriter(socket.getOutputStream(), true);
+     	in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+	}
+
+	private void parseArgs(String[] args) throws UnknownHostException {
+		host = InetAddress.getByName(args[0]);
+		port = Integer.parseInt(args[1]);
+		operation = args[2];
+		plateNumber = args[3];
+		int cipherSuitesFirstInd = 4;
+		if (operation.equals("REGISTER")) {
+			ownerName = args[4];
+			cipherSuitesFirstInd = 5;
+		}
+		
+		for (int i = cipherSuitesFirstInd; i < args.length; i++) {
+			wantedCipherSuites.add(args[i]);
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		SSLClient client = null;
+		try {
+			client = new SSLClient(args);
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		client.run();
+	}
+
+	private void run() {
+		switch (operation) {
+		case "REGISTER":
+			sendRegister();
+			break;
+		case "LOOKUP":
+			sendLookup();
+			break;
+		default:
+			System.err.println("Invalid operation.");
+		}
+		
+		receiveReply();
+	}
+
+	private void sendRegister() {
+		// TODO Auto-generated method stub
+		
+	}
 }
--- src/server/SSLServer.java
+++ src/server/SSLServer.java
@@ -1,5 +1,122 @@
 package server;
 
-public class SSLServer {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.security.PrivilegedActionException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 
+import javax.net.ssl.SSLServerSocket;
+import javax.net.ssl.SSLServerSocketFactory;
+import javax.net.ssl.SSLSocket;
+
+public class SSLServer {
+	
+	// (plate number, owner name)
+	private HashMap<String,String> storedData = new HashMap<String,String>();
+	
+	private SSLServerSocket serverSocket;
+	private int port;
+	private ArrayList<String> wantedCipherSuites;
+	
+	public SSLServer(String[] args) {
+		parseArgs(args);
+		initServerSocket();
+	}
+
+	private void parseArgs(String[] args) {
+		if (args.length < 1) {
+			printUsage();
+		}
+		port = Integer.parseInt(args[0]);
+		for (int i = 1; i < args.length; i++) {
+			wantedCipherSuites.add(args[i]);
+		}
+	}
+
+	private void initServerSocket() {
+		SSLServerSocketFactory ssf = (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();  
+		 
+		try {  
+			serverSocket = (SSLServerSocket) ssf.createServerSocket(port);  
+		}  
+		catch(IOException e) {  
+		    System.out.println("Server - Failed to create SSLServerSocket");  
+		    e.getMessage();  
+		    return;  
+		}
+		
+		serverSocket.setNeedClientAuth(true);
+		
+		//TODO: set wanted cipher suites
+		initServerCipherSuites();
+	}
+
+	private void initServerCipherSuites() {
+		String[] supportedSuites = serverSocket.getSupportedCipherSuites();
+		ArrayList<String> wantedSupportedSuites = new ArrayList<String>();
+		for (String wantedSuite : wantedCipherSuites) {
+			if (Arrays.asList(supportedSuites).contains(wantedSuite)) {
+				wantedSupportedSuites.add(wantedSuite);
+			}
+		}
+		
+		serverSocket.setEnabledCipherSuites(wantedSupportedSuites.toArray(new String[wantedSupportedSuites.size()]));
+	}
+
+	private void printUsage() {
+		System.out.println("java SSLServer <port> <cypher-suite>*");
+	}
+
+	private void run() {
+		while (true) {
+			serverIteration();
+		}
+	}
+
+
+	private void serverIteration() {
+		SSLSocket socket = null;
+		try {
+			socket = (SSLSocket) serverSocket.accept();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try {
+			// Create Input / Output Streams for communication with the client
+			while (true) {
+				PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+		        BufferedReader in = new BufferedReader(
+						new InputStreamReader(
+								socket.getInputStream()));
+		        String inputLine, outputLine;
+
+		        while ((inputLine = in.readLine()) != null) {
+		             out.println(inputLine);
+		             System.out.println(inputLine);
+		        }
+		        
+		        // Close the streams and the socket
+		        out.close();
+		        in.close();
+		        socket.close();
+			}
+		} catch (Exception exp) {
+			PrivilegedActionException priexp = new PrivilegedActionException(exp);
+			System.out.println(" Priv exp --- " + priexp.getMessage());
+
+			System.out.println(" Exception occurred .... " +exp);
+			exp.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		SSLServer server = new SSLServer(args);
+		server.run();
+	}
 }
