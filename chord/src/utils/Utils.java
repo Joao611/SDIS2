85e5de492eb1bee91bce14b7e15f3209bda5c3d6
AnabelaSilva <up201506034@fe.up.pt>
2018-05-07 13:56:55 +0100

    Log comecado

--- chord/src/utils/Utils.java
+++ chord/src/utils/Utils.java
@@ -1,6 +1,9 @@
 package utils;
 
+import java.io.IOException;
 import java.util.Random;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
 
 import chord.ChordManager;
 
@@ -10,6 +13,22 @@ public class Utils {
 	public static final String ENCODING_TYPE = "ISO-8859-1";
 	public static final int MAX_LENGTH_CHUNK = 64000;
 	public static final int BYTE_TO_KBYTE = 1000;
+	
+	static Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+	
+	static {
+		logger.setUseParentHandlers(false);
+		try {
+
+			logger.addHandler(new FileHandler("AnabelaSilvaLOG.txt", true));
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	
 	public static void randonSleep(int time) throws InterruptedException {
 		Random r = new Random();
@@ -35,6 +54,30 @@ public class Utils {
 	}
 	
 	public static void log(String message) {
-		
+		logger.info(message);
 	}
+	
+	
 }
+
+  
+//FileHandler fh;  
+//
+//try {  
+//
+//    // This block configure the logger with handler and formatter  
+//    fh = new FileHandler("C:/temp/test/MyLogFile.log");  
+//    logger.addHandler(fh);
+//    SimpleFormatter formatter = new SimpleFormatter();  
+//    fh.setFormatter(formatter);  
+//
+//    // the following statement is used to log any messages  
+//    logger.info("My first log");  
+//
+//} catch (SecurityException e) {  
+//    e.printStackTrace();  
+//} catch (IOException e) {  
+//    e.printStackTrace();  
+//}  
+
+//logger.info("Hi How r u?"); 
