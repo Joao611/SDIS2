802a67f67ce1725ec56d4acd2ee53dca12124444
AnabelaSilva <up201506034@fe.up.pt>
2018-05-10 12:47:19 +0100

    start

---
+++ chord/src/program/Leases.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package program;
+
+/**
+ * @author anabela
+ *
+ */
+public class Leases implements Runnable {
+
+	
+	Peer peer;
+	
+	
+	
+	@Override
+	public void run() {
+		
+	}
+
+}
