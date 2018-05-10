3e9b0c886a5bc91c5df176b5cc61f5437a8b895b
AnabelaSilva <up201506034@fe.up.pt>
2018-05-10 09:34:22 +0100

    Adicionei o derby

--- chord/run.sh
+++ chord/run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "Compiling...!"
-javac -d bin/ -cp src src/program/Peer.java
+javac -d bin/ -cp "derby.jar:src" src/program/Peer.java
 
 echo "Creating peers!"
-gnome-terminal -x java -cp bin program.Peer 9000
+gnome-terminal -x java -cp "derby.jar:bin" program.Peer 9000
 for (( c=1; c<3; c++ ))
 do  
-	gnome-terminal -x java -cp bin program.Peer 900$c localhost 9000
+	gnome-terminal -x java -cp "derby.jar:bin" program.Peer 900$c localhost 9000
 done
